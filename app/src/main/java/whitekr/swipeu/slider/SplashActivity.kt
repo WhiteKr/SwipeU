13c87384255b9715af2e4958c265e94d52a5ae89
WhiteKr <hanjisu2215g@gmail.com>
2021-12-03 13:00:56 +0900

    Add join logic on splash

--- app/src/main/java/whitekr/swipeu/slider/SplashActivity.kt
+++ app/src/main/java/whitekr/swipeu/slider/SplashActivity.kt
@@ -4,20 +4,39 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import whitekr.swipeu.MainActivity
 import whitekr.swipeu.R
 import whitekr.swipeu.auth.IntroActivity
+import whitekr.swipeu.utils.FirebaseAuthUtils
 
 class SplashActivity : AppCompatActivity() {
+
+    private val TAG: String = "SplashActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            val intent = Intent(this, IntroActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-            startActivity(intent)
-            finish()
-        }, 3000)
+        val uid = FirebaseAuthUtils.getUid()
+
+        if (uid == null) {
+
+            Handler().postDelayed({
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+                finish()
+            }, 3000)
+
+        } else {
+
+            Handler().postDelayed({
+                val intent = Intent(this, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+                finish()
+            }, 3000)
+
+        }
     }
 }
