347602a8fa81482a49f458aaae0066e0f79482a4
WhiteKr <hanjisu2215g@gmail.com>
2021-12-03 13:20:48 +0900

    Move to LoginActivity when the Login button clicked

--- app/src/main/java/whitekr/swipeu/auth/IntroActivity.kt
+++ app/src/main/java/whitekr/swipeu/auth/IntroActivity.kt
@@ -12,6 +12,14 @@ class IntroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
+        val loginBtn: Button = findViewById(R.id.loginBtn)
+        loginBtn.setOnClickListener {
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+
         val joinBtn: Button = findViewById(R.id.joinBtn)
         joinBtn.setOnClickListener {
 
