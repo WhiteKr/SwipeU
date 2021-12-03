fff6aceb0fbf76aa92f74536f654ebb469d4e019
WhiteKr <hanjisu2215g@gmail.com>
2021-12-03 09:35:52 +0900

    Add Join(register) activity

---
+++ app/src/main/java/whitekr/swipeu/auth/JoinActivity.kt
@@ -0,0 +1,14 @@
+package whitekr.swipeu.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import whitekr.swipeu.R
+
+class JoinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+
+}
